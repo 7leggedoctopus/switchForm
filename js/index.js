5944350eb229a0520e1e9318ec836ce2ca5b10cc
Vadym <106728132+7leggedoctopus@users.noreply.github.com>
2022-07-28 19:43:09 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,14 +5,19 @@
         function closeModal(modalId) {
             document.getElementById(modalId).style.display = 'none';
         }
+
         function swichAccept() {
             let arrSelect = document.getElementsByClassName('form-select');
             for (var i = 0; i < arrSelect.length; i++) {
                 arrSelect[i].selectedIndex = 0;
-                console.log(arrSelect[i]);
             }
             var arrCheck = document.getElementsByClassName('form-check-input');
             for (var i = 0; i < arrCheck.length; i++) {
-            arrCheck[i].checked = true;
+                arrCheck[i].checked = true;
+            }
+            var arrChips = document.getElementsByClassName('chip');
+            for (var i = 0; i < arrChips.length; i++) {
+                arrChips[i].style.display = '';
+                
             }
         }
